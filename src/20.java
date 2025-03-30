ad866d7db6a757f5cda4b022f0d01083c8445d69
alqassimisaid572 <alqassimisaid572@gmail.com>
2025-03-30 14:59:30 +0000

    Add 20.java

---
+++ src/20.java
@@ -0,0 +1,12 @@
+public class SchoolProject {
+    private String name;
+    private int year;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
