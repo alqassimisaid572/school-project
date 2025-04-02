7a8eb0f5a4fcf181aac72bbf460187d662c5fa11
alqassimisaid572 <alqassimisaid572@gmail.com>
2025-04-02 00:07:13 +0000

    Add 23.java

---
+++ src/23.java
@@ -0,0 +1,11 @@
+public class SchoolProject {
+    private String name;
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
