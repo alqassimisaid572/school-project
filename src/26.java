fc00c30ff48a4078d1e2f1a86f919b3411f23c55
alqassimisaid572 <alqassimisaid572@gmail.com>
2025-04-06 07:20:21 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,6 @@
+public class SchoolProjectExample {
+    public static void main(String[] args) {
+        // Code to be executed in Main method
+        System.out.println("Hello, World!");
+    }
+}
