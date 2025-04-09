f3c93f01e90f8d3d99a6f745c3006c570d85846f
alqassimisaid572 <alqassimisaid572@gmail.com>
2025-04-09 12:44:13 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,13 @@
+public class SchoolProjectRepository {
+    public void addStudent(Student student) {
+        // Add the student to the repository
+    }
+
+    public List<Student> getAllStudents() {
+        // Get all students from the repository and return a list of them
+    }
+
+    public boolean updateStudent(int id, Student updatedStudent) {
+        // Update an existing student with the given ID in the repository
+    }
+}
