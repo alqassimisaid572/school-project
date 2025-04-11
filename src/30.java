8c85deeac944851b2e6f5773357bdc7ac23ab4e2
alqassimisaid572 <alqassimisaid572@gmail.com>
2025-04-11 11:10:56 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,13 @@
+public class RandomCodeGenerator {
+    public static void main(String[] args) {
+        // Generate a random integer between 1 and 100
+        int randomInt = (int)(Math.random() * 100);
+        
+        // Generate a random string of 5 characters
+        String randomString = "Random";
+        
+        // Print the random integer and its corresponding string
+        System.out.println("Random Int: " + randomInt);
+        System.out.println("Generated Random String: " + randomString);
+    }
+}
