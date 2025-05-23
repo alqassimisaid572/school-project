81a1ff219b51381d89dd2298695c0c0878cb2e67
alqassimisaid572 <alqassimisaid572@gmail.com>
2025-05-23 07:02:23 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,16 @@
+public class SchoolProject {
+    private int id;
+    private String name;
+
+    public SchoolProject(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Add your code here
+
+    public void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+}
