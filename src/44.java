f32df97598a110622d89b8e66ada05fabbfb40a5
alqassimisaid572 <alqassimisaid572@gmail.com>
2025-05-27 01:31:48 +0000

    Add 44.java

---
+++ src/44.java
@@ -0,0 +1,7 @@
+public class SchoolProjectRepository {
+    public void logInfo(String message) {
+        System.out.println(message);
+    }
+
+    // Assuming there is another method in this class named `logError` which should be implemented.
+}
